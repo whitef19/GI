67f070e6f234c0cd21ae57ca80b7a563ead9b4ef
whitef <frederique.white@usherbrooke.ca>
2018-08-02 14:33:55 -0400

    reference.py

--- IslandPick/EDirect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for gi in `cat $1`
-do
-	echo "$gi"
-	#esearch -db pubmed -query "$gi" -sort Relevance |efetch -format medline | grep -wf pattern >>output/EDirect_references
-done
-
-echo "DONE"
---
+++ IslandPick/references.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# python 3.5
+import os
+import sys
+import csv
+import argparse
+import datetime
+import pandas as pd
+import _pickle as cpickle
+
+class Ilot:
+	def __init__(self,line,col,desired):
+		self.col=col
+		self.ID=line[col.index('ACCESSION')]+'_'+str(line[col.index('START')])+'_'+str(line[col.index('END')])
+		self.line=line
+		self.positif=False
+		if 'REFERENCE' in col:
+			self.positif=True 
+		
+	def get_ajusted(self,desired):
+		self.ajusted=[str(self.line[self.col.index(c)]) if c in self.col else '' for c in desired]
+		return self.ajusted
+	def __eq__ (self, other):
+		return self.ID==other.ID
+	def __eq__ (self, ID):
+		return self.ID==ID
+
+def timestamp(name):
+	print("{0} : Timestamp: {1:%Y-%m-%d %H:%M:%S}".format(name, datetime.datetime.now()))
+
+def argsparse():
+	parser=argparse.ArgumentParser(description='Parsing of genomic island database')
+	parser.add_argument('-q','--query', action='store', dest='query', help='accession,organism,start,end,reference',required=True)
+	parser.add_argument('-p','--pickle', action='store',dest='pickle',help='pickle file of genomic island',required=True)
+	parser.add_argument('-o','--output' action='store', dest='output',help='output file (default=output.out)',default='output.out')
+	parser.add_argument('-key', action='store', dest='key',help='keyword for research')
+	args=parser.parse_args()
+	return args
+
+def pickle(name,objects) :
+	with open(name,'wb') as file:
+		pickler = cpickle.Pickler(file)
+		pickler.dump(objects)
+
+def unpickle(name):
+	with open(name,'rb') as f:
+		pickler=cpickle.Unpickler(f)
+		objects=pickler.load()
+	return objects
+
+def writing(desired,strings,output):
+	o=open('EDirect.sh','w') 
+	o.write('#!/bin/bash'+'\n')
+	for string in strings:
+		o.write('\n'+'echo "#'+string+'" >> '+output)
+		o.write('\n'+'esearch -db pubmed -query "'+string+'" -sort Relevance |efetch -format medline | grep -wf pattern >> '+output)
+	o.close()
+
+def main():
+	args=argsparse()
+	islands=unpicle(args.pickle)
+	desired=['ACCESSION','ORGANISM','START','END','SEQUENCE','INSERTION','REFERENCE','DETECTION']
+	stings=[]
+	for island in islands:
+		line=island.get_ajusted(desired)
+		line[1]=line[1].split(',')[0]
+		string=(' '.join([line[i] for i,info in enumerate(desired)] if info in args.query))
+		if args.key:
+			string=string+' '+' '.join(args.key)
+		strings.append(sting)
+	writing(desired,strings,args.output)
+
+if __name__ == "__main__":
+	timestamp("STARTING")
+	main()
+	timestamp("DONE")
